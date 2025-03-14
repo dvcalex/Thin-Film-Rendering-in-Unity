6932aad77eb24ea88881cbd8d689fdd1e2970a3a
alex_dvc <115949328+TrumpetYoda@users.noreply.github.com>
2025-03-13 20:51:53 -0400

    fixed original cubemap, BUT reflection is reflecting the old cubemap?
    Also did some other reorganizing and added a datatexture manager so we can update textures only when we want to.

---
+++ Assets/Scripts/DataTextureManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+
+[ExecuteAlways]
+public class DataTextureManager : MonoBehaviour
+{
+    public static DataTextureManager Instance = null;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+    
+    public delegate void OnRecalculateTexture();
+    public OnRecalculateTexture onRecalculateTexture;
+    
+    [SerializeField] private bool recalculateTextureOnce = false;
+    [SerializeField] private bool recalculateTextureConstant = false;
+
+    private void Update()
+    {
+        if (recalculateTextureOnce || recalculateTextureConstant)
+        {
+            recalculateTextureOnce = false;
+            onRecalculateTexture?.Invoke();
+        }
+    }
+}
